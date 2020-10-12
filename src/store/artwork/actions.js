3614b0ed36c68935eb0de25e88c2563892e60c3a
reneeduijzers <reneeduijzers@hotmail.com>
2020-10-12 12:58:10 +0200

    Merge branch 'master' of github.com:reneeduijzers/artwork_store Updated README at Github

--- src/store/artwork/actions.js
+++ src/store/artwork/actions.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 export async function fetchArtworks(dispatch, getState) {
   const { artwork } = getState();
   if (!artwork.length) {
-    const response = await Axios.get("https://artwork-server.herokuapp.com");
+    const response = await Axios.get("http://localhost:4000");
     console.log("did the API handle the get artworks request?", response);
     const artworks = response.data;
     dispatch({ type: "ALL_ARTWORKS", payload: artworks });
